65a8770f63a7e0d1ec136b646a1f88383b693057
Eng-Hari <hariharan03.@gmail.com>
2025-11-26 22:21:35 +0530

    Magic Numbers

---
+++ MagicNum.java
@@ -0,0 +1,63 @@
+
+import java.util.*;
+
+public class MagicNum {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        boolean home_page = true;
+        int attempt = 5;
+        // int sys = (int) (Math.random() * 100);
+        int sys = 5;
+        do {
+            System.out.println("Vannakam to Magic Number");
+            System.out.println("------------------------");
+            boolean game = true;
+            do {
+                if (attempt >= 1) {
+                    System.out.print("Enter your choice : ");
+                    int n = sc.nextInt();
+                    attempt--;
+                    if (n > sys) {
+                        System.out.println("Its High");
+                    } else if (n < sys) {
+                        System.out.println("Its Low");
+                    } else if (sys == n) {
+                        System.out.println("\nYou got it Buddy...!\n");
+                        // attempt = 5;
+                        game = false;
+                        Thread.sleep(2000);
+
+                        System.out.println("\n1. Re-Start\n2. Exit");
+                        int opp = sc.nextInt();
+
+                        if (opp == 1) {
+                            System.out.println("Processing....\n");
+                            Thread.sleep(3000);
+                            attempt = 5;
+                            // sys = (int) (Math.random() * 100);
+                            sys = 5;
+
+                        } else if (opp == 2) {
+                            game = false;
+                            System.out.println("Game over, Visit Again..!\n");
+                            home_page = false;
+                        }
+
+                    }
+
+                } else if (attempt == 0) {
+
+                    game = false;
+                    home_page = false;
+                    System.out.println("\nCross the limit, Try again..! \n   Thank you ..:)");
+
+                    // home_page = false;
+                }
+
+            } while (game);
+        } while (home_page);
+    }
+}
